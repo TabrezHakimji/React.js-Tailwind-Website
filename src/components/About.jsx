9e274d69d170b9843e2f9d3520b1fbbe2d4d7cc3
Tabrezhira <Tabrez.hira@gmail.com>
2024-03-11 10:23:04 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import about from '../assets/about.png';
+
+const About = () => {
+  return ( 
+    <div>
+        {/* about text*/}
+        <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto my-8'>
+            <div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
